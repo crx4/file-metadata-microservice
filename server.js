72f5edb7f9d7ae1ce61064b93106beec271f067c
Mevlut Canvar <mevlutcanvar@gmail.com>
2020-10-02 22:42:51 +0000

    v1.0.0

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var express = require('express');
 var cors = require('cors');
 
 // require and use "multer"...
+var multer  = require('multer');
+var upload = multer({ dest: 'uploads/' })
 
 var app = express();
 
@@ -14,9 +16,15 @@ app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
   });
 
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
-});
+app.post(
+  '/api/fileanalyse', 
+  upload.single('upfile'), 
+  (req, res) => res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+);
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
